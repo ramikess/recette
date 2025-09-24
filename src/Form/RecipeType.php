9e109d31f34a5e28b655d0143a247ace66a2b399
kessentini <rami.kessentini@poudebon.com>
2025-09-24 16:55:14 +0200

    list ingredients

--- src/Form/RecipeType.php
+++ src/Form/RecipeType.php
@@ -6,6 +6,7 @@ use App\Entity\Recipe;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,8 +15,12 @@ class RecipeType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('content', TextareaType::class)
+            ->add('name', TextType::class, [
+                'label' => 'Nom',
+            ])
+            ->add('content', TextareaType::class, [
+                'label' => 'Contenu',
+            ])
             ->add('ingredients', CollectionType::class, [
                 'label' => false,
                 'entry_type' => IngredientType::class,
